99aacbf5092b4b644c2fa98c9ddb275ea67c47e9
Matthias Mair <code@mjmair.com>
2025-04-22 01:33:17 +0200

    update site_url

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = ["adr/template.md"]
 
-ogp_site_url = "https://project.inventree.org/"
+ogp_site_url = "https://matmair.github.io/project"
 html_theme = "furo"
 html_static_path = ["_static"]
 html_theme_options = {
