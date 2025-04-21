b9d4a9eebd1006fed2c9fd03ee558a6521b25b80
Matthias Mair <code@mjmair.com>
2025-04-22 01:12:11 +0200

    format file

--- docs/conf.py
+++ docs/conf.py
@@ -6,26 +6,26 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'InvenTree Project'
-copyright = 'InvenTree contributors'
-author = 'Matthias Mair <code@mjmair.com>'
-release = '0.1.0'
-html_title = 'InvenTree Project Documentation'
+project = "InvenTree Project"
+copyright = "InvenTree contributors"
+author = "Matthias Mair <code@mjmair.com>"
+release = "0.1.0"
+html_title = "InvenTree Project Documentation"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-     'sphinxext.opengraph',
-     "myst_parser",
+    "sphinxext.opengraph",
+    "myst_parser",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['adr/template.md']
+templates_path = ["_templates"]
+exclude_patterns = ["adr/template.md"]
 
-ogp_site_url = 'https://project.inventree.org/'
-html_theme = 'furo'
-html_static_path = ['_static']
+ogp_site_url = "https://project.inventree.org/"
+html_theme = "furo"
+html_static_path = ["_static"]
 html_theme_options = {
     "announcement": "<em>In Beta</em> This is only in testing, not official yet!",
     "source_repository": "https://github.com/matmair/project",
